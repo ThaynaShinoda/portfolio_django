2a044c58a7b472ea5596636117c96f0d7dc6f59d
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2025-07-28 14:37:15 -0300

    fix: slug

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project
 
-admin.site.register(Project)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('title', 'slug', 'status', 'created_on')
+  list_filter = ('status',)
+  search_fields = ['title', 'description']
+  prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Project, PostAdmin)
